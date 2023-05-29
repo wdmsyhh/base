6ba2ca9b6cb1959ba850dba9eef8362f2a85889b
Ken Yin <ken.yin@maiscrm.com>
2023-05-29 11:10:45 +0800

    refine

--- base-ubuntu/buildImage.sh
+++ base-ubuntu/buildImage.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-ENV=${ENV:-local}
+ENVS=${ENVS:-"local,staging,production"}
+ENVS=${ENVS//,/ }
 REGISTRY_HOST=${REGISTRY_HOST:-"registry.ap-southeast-1.aliyuncs.com"}
 REGISTRY_REPOSITORY=${REGISTRY_REPOSITORY:-"yhhnamespace/base-ubuntu"}
 IMAGE_NAME=${REGISTRY_HOST}/${REGISTRY_REPOSITORY}
@@ -11,4 +12,7 @@ buildImage() {
   docker push ${imageNameWithTag}
 }
 
-buildImage
+for env in ${ENVS}; do
+  export ENV=${env}
+  buildImage
+done
